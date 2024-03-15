fd0b97366e8ee05d1350e507716c7a240191c303
Hyerin Kim <kdjsanh0217@naver.com>
2024-03-15 19:14:50 +0900

    로딩스피너 추가

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
 import Spinner from 'react-bootstrap/Spinner';
 
 const Skeleton = () => {
   return (
-    <Spinner animation="border" role="status" style={{display:'flex', justifyContent:'center'}}>
-      <span className="visually-hidden">Loading...</span>
-    </Spinner>
-  )
-}
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Spinner animation="border" role="status" style={{ width: '5rem', height: '5rem' }}>
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </div>
+  );
+};
 
-export default Skeleton
+export default Skeleton;
